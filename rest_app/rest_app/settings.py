db7a0f90c9f25370e883d1f9d1c7bab4105c22e0
OlgaPaw <olga.pawlowicz@gmail.com>
2018-11-29 12:49:18 +0100

    Add REST extension

--- rest_app/rest_app/settings.py
+++ rest_app/rest_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o)cdxj9(x^$3x2t!@q)fssos0ik50=_lrpj%s#6xg76o+6q%xq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
